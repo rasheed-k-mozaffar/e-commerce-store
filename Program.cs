5beb0a910ea1c3ddbc236dbb2e2ba723cebd69fc
Ismael <ismael.ka@protonmail.com>
2023-10-16 14:14:41 +0300

    add new sqlLite db for test

--- Program.cs
+++ Program.cs
@@ -9,11 +9,11 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 // builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//         options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+//         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
